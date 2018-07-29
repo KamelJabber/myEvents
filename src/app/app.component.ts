daa61afa37295dcf3062e414f25d5d2ccecbf996
KamelJabber <kameljabber@mail.com>
2018-07-29 12:03:26 -0500

    Auto reload application via SwUpdate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'myev-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  constructor(swUpdate: SwUpdate) {
+    swUpdate.available.subscribe(() => {
+      // auto refresh
+      // TODO: make as an option for RELEASE
+
+      swUpdate.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
